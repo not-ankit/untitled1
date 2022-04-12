e714beb61eb9a856e849cbc469910e86689d7113
Ankit <ankit.thakur@developmentlogics.com>
2022-04-12 15:01:49 +0530

    tuesdayfinalpush

--- lib/states/mainState.dart
+++ lib/states/mainState.dart
@@ -1,3 +1,4 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled1/bottomNavScreens/navHome.dart';
 import 'package:untitled1/bottomNavScreens/navMenu.dart';
 import 'package:untitled1/bottomNavScreens/navNotifications.dart';
@@ -7,6 +8,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 class mainState extends State
 {
+  static var menuItemsList = ["","Attendance","Leave","Profile"];
+  static var menuAssets = ["","assets/attendance.svg","assets/leave.svg","assets/leave.svg"];
   GlobalKey<ScaffoldState> key = new GlobalKey();
   int pageIndex = 0;
   @override
@@ -20,23 +23,33 @@ class mainState extends State
     var teamsIcon = SvgPicture.asset("assets/teams.svg");
     var notiIcon = SvgPicture.asset("assets/notification.svg");
     var menuIcon = SvgPicture.asset("assets/menu.svg");
-    return Scaffold(
-      key:key,
-      backgroundColor: const Color(0xffE5E5E5),
-      body:pageArray[pageIndex],
-      endDrawer: Drawer(backgroundColor: Colors.white,
-      child: Column(children: [
-        Container(),
-        ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return drawerListChild();
-        },)
-      ],)),
-      bottomNavigationBar: BottomNavigationBar(currentIndex: pageIndex,backgroundColor: Colors.white,selectedIconTheme: selectedIconTheme,unselectedIconTheme: unselectedIconTheme,selectedItemColor: cardColor,unselectedItemColor: Colors.grey,showSelectedLabels: true,showUnselectedLabels: true,onTap: bottomOnTap,items: [BottomNavigationBarItem(icon: homeIcon,label: "Home"),
-      BottomNavigationBarItem(icon: teamsIcon,label:"My Teams"),
-      BottomNavigationBarItem(icon: notiIcon,label: "Notifications"),
-      BottomNavigationBarItem(icon: menuIcon,label: "Menu"),
-    ],),
-    );
+      return Scaffold(
+        key: key,
+        backgroundColor: const Color(0xffE5E5E5),
+        body: pageArray[pageIndex],
+        endDrawer: Drawer(backgroundColor: Colors.white,
+            child: Column(children: [
+              Container(),
+              Container(height: MediaQuery.of(context).size.height,width: double.infinity,child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return drawerListChild(index);
+                },itemCount: menuItemsList.length,))
+            ],)),
+        bottomNavigationBar: BottomNavigationBar(currentIndex: pageIndex,
+          backgroundColor: Colors.white,
+          selectedIconTheme: selectedIconTheme,
+          unselectedIconTheme: unselectedIconTheme,
+          selectedItemColor: cardColor,
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          onTap: bottomOnTap,
+          items: [BottomNavigationBarItem(icon: homeIcon, label: "Home"),
+            BottomNavigationBarItem(icon: teamsIcon, label: "My Teams"),
+            BottomNavigationBarItem(icon: notiIcon, label: "Notifications"),
+            BottomNavigationBarItem(icon: menuIcon, label: "Menu"),
+          ],),
+      );
   }
   void bottomOnTap(int clickedId)
   {
@@ -51,13 +64,31 @@ class mainState extends State
 }
 class drawerListChild extends StatelessWidget
 {
+  drawerListChild(this.index, {Key? key}) : super(key: key);
+  int index;
   @override
   Widget build(BuildContext context) {
-    return Container(height: 30,width:double.infinity,child: Padding(padding: const EdgeInsets.only(left: 20),child:Row(crossAxisAlignment: CrossAxisAlignment.center,children: [
-      SvgPicture.asset("user.svg"),
-      SizedBox(height: 30,width: 10,),
-      Text("Attendance")
-    ],)),);
+    switch(index)
+    {
+      case 0:
+        return Stack(children: [
+          Padding(padding: EdgeInsets.only(left: 30,top:30),child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [
+            Container(child:SvgPicture.asset("assets/Logo.svg")),
+            SizedBox(width: double.infinity,height: 20,),
+            Text("Development Logics",style:GoogleFonts.poppins(fontSize: 18,fontWeight: FontWeight.w500,color:const Color(0xff484848))),
+            SizedBox(width: double.infinity,height: 5,),
+            Text("Workspace",style:GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.w400,color:Color(0xff696969))),
+            SizedBox(height: 30,width: double.infinity,)
+          ],))
+        ],);
+      default:
+        return Container(height: 60,width:double.infinity,child: Padding(padding: const EdgeInsets.only(left: 20),
+          child:Row(crossAxisAlignment: CrossAxisAlignment.center,children: [
+            SvgPicture.asset(mainState.menuAssets[index]),
+            SizedBox(height: 30,width: 10,),
+            Text(mainState.menuItemsList[index])
+          ],)),);
+    };
   }
 }
 
